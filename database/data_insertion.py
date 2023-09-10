db0efda288431c005d935411da85b0960715c1e4
pupersosition <nikita.vostrosablin@gmail.com>
2023-09-10 14:17:39 +0200

    fixed data insertion

--- database/data_insertion.py
+++ database/data_insertion.py
@@ -1,125 +1,135 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float, UniqueConstraint, and_, DECIMAL
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float, DECIMAL, UniqueConstraint, and_, BigInteger
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 from sqlalchemy.exc import OperationalError
+from sqlalchemy.dialects.postgresql import insert
 import pandas as pd
 import os
+import time
+from contextlib import contextmanager
+from copy import deepcopy
 
 # Configuration
-print("Loading configurations...")
 DB_HOST = os.environ['DB_HOST']
 DB_NAME = os.environ['DB_NAME']
 DB_USER = os.environ['DB_USER']
 DB_PASS = os.environ['POSTGRES_PASSWORD']
 CSV_PATH = os.environ['CSV_PATH']
+BUFFER_SIZE = 5000
 MAX_ATTEMPTS = 5
 SLEEP_INTERVAL = 5  # in seconds
 
 Base = declarative_base()
 
-
 class Manufacturer(Base):
     __tablename__ = 'manufacturers'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, unique=True)
     cars = relationship("Car", back_populates="manufacturer")
 
-
-class City(Base):
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    city_url = Column(String)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    cars = relationship("Car", back_populates="city")
-    __table_args__ = (UniqueConstraint('name', 'city_url', name='city_city_url_uc'),)
-
-
 class Car(Base):
-    __tablename__ = 'craigslist_cars'
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'cars'
+    id = Column(BigInteger, primary_key=True)
     url = Column(String)
     price = Column(DECIMAL)
     year = Column(Integer)
-    make = Column(String)
+    model = Column(String)
     condition = Column(String)
     cylinders = Column(String)
     fuel = Column(String)
     odometer = Column(DECIMAL)
     title_status = Column(String)
     transmission = Column(String)
-    VIN = Column(String)
+    vin = Column(String)
     drive = Column(String)
     size = Column(String)
     type = Column(String)
     paint_color = Column(String)
     image_url = Column(String)
     description = Column(String)
+    latitude = Column(Float)
+    longitude = Column(Float)
     manufacturer_id = Column(Integer, ForeignKey('manufacturers.id'))
     manufacturer = relationship("Manufacturer", back_populates="cars")
-    city_id = Column(Integer, ForeignKey('cities.id'))
-    city = relationship("City", back_populates="cars")
-
 
 def create_session():
-    print("Attempting to connect to the database...")
     for attempt in range(MAX_ATTEMPTS):
         try:
             engine = create_engine(f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}')
             Session = sessionmaker(bind=engine)
-            print("Connected successfully!")
             return Session()
         except OperationalError:
-            print(f"Attempt {attempt + 1} failed. Retrying in {SLEEP_INTERVAL} seconds...")
             time.sleep(SLEEP_INTERVAL)
-    print(f"Failed to connect to the database after {MAX_ATTEMPTS} attempts.")
-    exit(1)
+    raise Exception(f"Failed to connect to the database after {MAX_ATTEMPTS} attempts.")
 
-
-session = create_session()
-
-print(f"Loading data from {CSV_PATH}...")
-df = pd.read_csv(CSV_PATH)
-
-print("Populating the database...")
-for index, row in df.iterrows():
-    print(f"Processing row {index}...")
-    manufacturer = session.query(Manufacturer).filter_by(name=row['manufacturer']).first()
-    if not manufacturer:
-        manufacturer = Manufacturer(name=row['manufacturer'])
-        session.add(manufacturer)
+@contextmanager
+def session_scope():
+    session = create_session()
+    try:
+        yield session
         session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
-    city = session.query(City).filter(and_(City.name == row['city'], City.city_url == row['city_url'])).first()
-    if not city:
-        city = City(name=row['city'], city_url=row['city_url'], latitude=row['lat'], longitude=row['long'])
-        session.add(city)
-        session.commit()
+def bulk_insert_avoid_conflicts(session, table, data):
+    if not data:
+        return
+    insert_stmt = insert(table).values(data)
+    on_conflict_stmt = insert_stmt.on_conflict_do_nothing()
+    session.execute(on_conflict_stmt)
 
-    car = Car(
-        url=row['url'],
-        price=row['price'],
-        year=row['year'],
-        make=row['make'],
-        condition=row['condition'],
-        cylinders=row['cylinders'],
-        fuel=row['fuel'],
-        odometer=row['odometer'],
-        title_status=row['title_status'],
-        transmission=row['transmission'],
-        VIN=row['VIN'],
-        drive=row['drive'],
-        size=row['size'],
-        type=row['type'],
-        paint_color=row['paint_color'],
-        image_url=row['image_url'],
-        description=row['desc'],
-        manufacturer=manufacturer,
-        city=city
-    )
-    session.add(car)
-    session.commit()
+def clean_data(record):
+    """Replace all 'nan' values with None for database compatibility."""
+    return {k: None if pd.isna(v) else v for k, v in record.items()}
 
-print("Data has been successfully added to the database!")
-session.close()
+# Processing manufacturers
+unique_manufacturers = pd.read_csv(CSV_PATH, usecols=['manufacturer'])['manufacturer'].dropna().unique().tolist()
+with session_scope() as session:
+    bulk_insert_avoid_conflicts(session, Manufacturer.__table__, [{"name": manufacturer} for manufacturer in unique_manufacturers])
+
+# Processing cars
+all_car_mappings = []
+chunk_iter = pd.read_csv(CSV_PATH, chunksize=BUFFER_SIZE)
+for chunk in chunk_iter:
+    car_objects = []
+
+    with session_scope() as session:
+        manufacturer_map = {manufacturer.name: manufacturer.id for manufacturer in session.query(Manufacturer).all()}
+        for _, row in chunk.iterrows():
+            car_mapping = {
+                'id': row['id'],
+                'url': row['url'],
+                'price': row['price'],
+                'year': row['year'],
+                'model': row['model'],
+                'condition': row['condition'],
+                'cylinders': row['cylinders'],
+                'fuel': row['fuel'],
+                'odometer': row['odometer'],
+                'title_status': row['title_status'],
+                'transmission': row['transmission'],
+                'vin': row['VIN'],
+                'drive': row['drive'],
+                'size': row['size'],
+                'type': row['type'],
+                'paint_color': row['paint_color'],
+                'image_url': row['image_url'],
+                'description': row['description'],
+                'latitude': row['lat'],
+                'longitude': row['long'],
+                'manufacturer': row['manufacturer']
+            }
+            car_mapping = clean_data(car_mapping)
+            if car_mapping.get('manufacturer') in manufacturer_map:
+                car_mapping['manufacturer_id'] = manufacturer_map[car_mapping['manufacturer']]
+            else:
+                car_mapping['manufacturer_id'] = None
+            if 'manufacturer' in car_mapping:
+                del car_mapping['manufacturer']
+
+            car = Car(**car_mapping)
+            car_objects.append(car)
+
+        session.bulk_save_objects(car_objects)
--- database/init_db.sql
+++ database/init_db.sql
@@ -1,28 +1,17 @@
 -- Create the manufacturers table
 CREATE TABLE manufacturers (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL
 );
 
--- Create the cities table
-CREATE TABLE cities (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    city_url TEXT,
-    latitude DECIMAL,
-    longitude DECIMAL,
-    UNIQUE(name, city_url) -- Ensuring that the combination of name and city_url is unique
-);
-
 -- Create the craigslist_cars table
-CREATE TABLE craigslist_cars (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE cars (
+    id BIGINT PRIMARY KEY,
     url TEXT,
-    city_id INTEGER REFERENCES cities(id),
     price DECIMAL,
     year INTEGER,
-    manufacturer_id INTEGER REFERENCES manufacturers(id),
-    make TEXT,
+    manufacturer_id BIGINT REFERENCES manufacturers(id),
+    model TEXT,
     condition TEXT,
     cylinders TEXT,
     fuel TEXT,
@@ -35,5 +24,8 @@ CREATE TABLE craigslist_cars (
     type TEXT,
     paint_color TEXT,
     image_url TEXT,
-    description TEXT
+    description TEXT,
+    latitude DECIMAL,
+    longitude DECIMAL,
+    posting_date TIMESTAMP
 );
